11e24d7baa508d93a5ddc008e9172935fac658c1
codingiove <2023221317@qq.com>
2023-03-07 14:22:20 +0800

    解决设置 TOC 目录模块的宽度无效问题

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@ npm install
 
 git clone https://github.com/EvanNotFound/hexo-theme-redefine.git themes/redefine
 
+# 覆盖源文件（解决设置 TOC 目录模块的宽度无效问题）
+cp overlay-article-content.styl themes/redefine/source/css/layout/article-content.styl
+
+# 资源压缩
+npm install hexo-all-minifier
+
 # 本地测试用的
 # hexo g
 # hexo server
